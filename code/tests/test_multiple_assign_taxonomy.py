567e9bbbac2df6b37f4672d0754ce178d40f3d6b
Jai Ram Rideout <jai.rideout@gmail.com>
2012-08-01 15:29:41 -0700

    Added more tests for error handling cases.

--- code/tests/test_multiple_assign_taxonomy.py
+++ code/tests/test_multiple_assign_taxonomy.py
@@ -12,7 +12,15 @@ __status__ = "Development"
 
 """Test suite for the multiple_assign_taxonomy.py module."""
 
+from os import makedirs, getcwd, chdir
+from os.path import exists
+from shutil import rmtree
+from tempfile import mkdtemp, NamedTemporaryFile
+from cogent.util.misc import remove_files
 from cogent.util.unit_test import TestCase, main
+from qiime.test import initiate_timeout, disable_timeout
+from qiime.util import get_qiime_temp_dir, get_tmp_filename
+from qiime.workflow import WorkflowError
 
 from taxcompare.multiple_assign_taxonomy import (
         assign_taxonomy_multiple_times, _generate_rdp_commands,
@@ -22,12 +30,73 @@ class MultipleAssignTaxonomyTests(TestCase):
     """Tests for the multiple_assign_taxonomy.py module."""
 
     def setUp(self):
-        """Define some sample data that will be used by the tests."""
-        pass
+        """Set up files/environment that will be used by the tests."""
+        # The prefix to use for temporary files. This prefix may be added to,
+        # but all temp dirs and files created by the tests will have this
+        # prefix at a minimum.
+        self.prefix = 'multiple_assign_taxonomy_tests'
+
+        self.start_dir = getcwd()
+        self.dirs_to_remove = []
+        self.files_to_remove = []
+        
+        self.tmp_dir = get_qiime_temp_dir()
+        if not exists(self.tmp_dir):
+            makedirs(self.tmp_dir)
+            # if test creates the temp dir, also remove it
+            self.dirs_to_remove.append(self.tmp_dir)
+
+        initiate_timeout(60)
+
+    def tearDown(self):
+        """ """
+        disable_timeout()
+
+        # change back to the start dir - some workflows change directory
+        chdir(self.start_dir)
+
+        remove_files(self.files_to_remove)
+        # remove directories last, so we don't get errors
+        # trying to remove files which may be in the directories
+        for d in self.dirs_to_remove:
+            if exists(d):
+                rmtree(d)
 
     def test_assign_taxonomy_multiple_times(self):
         """Functions correctly using standard valid input data."""
         pass
+
+    def test_assign_taxonomy_multiple_times_invalid_input(self):
+        """Test that errors are thrown using various types of invalid input."""
+        # The output directory already exists, and we aren't in force mode.
+        output_dir = mkdtemp(dir=self.tmp_dir,
+                             prefix='%s_output_dir_' %self.prefix)
+        self.dirs_to_remove.append(output_dir)
+
+        self.assertRaises(WorkflowError, assign_taxonomy_multiple_times,
+                ['/foo', '/bar/'], output_dir, ['rdp', 'blast'],
+                '/foo/ref_seqs.fasta', 'in.fasta', 'otu.biom')
+
+        # The input directories don't exist.
+        self.assertRaises(WorkflowError, assign_taxonomy_multiple_times,
+                ['/foobarbaz', '/foobarbaz2/'], output_dir, ['rdp', 'blast'],
+                '/foo/ref_seqs.fasta', 'in.fasta', 'otu.biom', force=True)
+
+        # Invalid assignment method.
+        self.assertRaises(WorkflowError, assign_taxonomy_multiple_times,
+                [output_dir, output_dir], output_dir, ['foo', 'rdp'],
+                '/foo/ref_seqs.fasta', 'in.fasta', 'otu.biom', force=True)
+
+        # RDP ID to taxonomy map is missing.
+        self.assertRaises(WorkflowError, assign_taxonomy_multiple_times,
+                [output_dir, output_dir], output_dir, ['rdp', 'blast'],
+                '/foo/ref_seqs.fasta', 'in.fasta', 'otu.biom', force=True)
+
+        # RDP confidences are missing.
+        self.assertRaises(WorkflowError, assign_taxonomy_multiple_times,
+                [output_dir, output_dir], output_dir, ['rdp', 'blast'],
+                '/foo/ref_seqs.fasta', 'in.fasta', 'otu.biom',
+                rdp_id_to_taxonomy_fp='/foo/id_to_tax.txt', force=True)
 
     def test_generate_rdp_commands(self):
         """Functions correctly using standard valid input data."""
