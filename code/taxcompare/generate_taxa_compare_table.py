6845839374a253e6f8ee2b1ec73ceae41bf098e6
Greg Caporaso <gregcaporaso@gmail.com>
2013-06-15 09:35:57 -0700

    modified to improve specificity of file search

--- code/taxcompare/generate_taxa_compare_table.py
+++ code/taxcompare/generate_taxa_compare_table.py
@@ -137,7 +137,9 @@ def generate_taxa_compare_table(root, key_directory, levels=None):
 
                 for f in files:
                     if ('otu_table_mc2_no_pynast_failures_w_taxa_L' in f or
-                        'otu_table_mc2_w_taxa_L' in f) and not f.endswith('~'):
+                        'otu_table_mc2_w_taxa_L' in f)\
+                        and not f.endswith('~')\
+                        and not f.endswith('.biom'):
                         name = path.split('/')[-2]
                         level = int(f[-5])
                         if level not in levels:
