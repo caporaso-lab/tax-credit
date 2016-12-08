98ec6ab07d6ad479fc248879a0f68c7053d320a3
Nicholas Bokulich <nbokulich@gmail.com>
2016-12-08 14:56:07 -0600

    Update taxa_manipulator.py

--- tax_credit/taxa_manipulator.py
+++ tax_credit/taxa_manipulator.py
@@ -2,7 +2,7 @@
 
 
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, taxcompare development team.
+# Copyright (c) 2016--, tax-credit development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
